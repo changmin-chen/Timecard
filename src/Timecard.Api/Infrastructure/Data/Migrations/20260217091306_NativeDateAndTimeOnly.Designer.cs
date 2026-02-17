b421b857f495699338c62d4ec734c88bd8d39ead
Chen, Chang-Min <47084071+changmin-chen@users.noreply.github.com>
2026-02-17 17:23:16 +0800

    Switch `DateOnly` and `TimeOnly` fields to native PostgreSQL `date` and `time` types, add constraints to enforce data integrity

--- src/Timecard.Api/Infrastructure/Data/Migrations/20260217091306_NativeDateAndTimeOnly.Designer.cs
+++ src/Timecard.Api/Infrastructure/Data/Migrations/20260217091306_NativeDateAndTimeOnly.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Timecard.Api.Infrastructure.Data;
@@ -11,9 +12,11 @@ using Timecard.Api.Infrastructure.Data;
 namespace Timecard.Api.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(TimecardDb))]
-    partial class TimecardDbModelSnapshot : ModelSnapshot
+    [Migration("20260217091306_NativeDateAndTimeOnly")]
+    partial class NativeDateAndTimeOnly
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
